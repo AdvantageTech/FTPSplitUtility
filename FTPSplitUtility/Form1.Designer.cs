dce7719f8293f4228bf0802ecad0c8577d48ecdd
Tom Adkins <tomadkinsii@gmail.com>
2019-11-25 13:39:13 -0500

    Add project files.

---
+++ FTPSplitUtility/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace FTPSplitUtility
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSplitPath = new System.Windows.Forms.TextBox();
+            this.btnSplitPath = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSplitName = new System.Windows.Forms.TextBox();
+            this.btnSplitFiles = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(710, 42);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(37, 30);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(573, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select PDF File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Split Path:";
+            // 
+            // textSplitPath
+            // 
+            this.textSplitPath.Location = new System.Drawing.Point(131, 103);
+            this.textSplitPath.Name = "textSplitPath";
+            this.textSplitPath.Size = new System.Drawing.Size(573, 20);
+            this.textSplitPath.TabIndex = 4;
+            // 
+            // btnSplitPath
+            // 
+            this.btnSplitPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSplitPath.Location = new System.Drawing.Point(710, 98);
+            this.btnSplitPath.Name = "btnSplitPath";
+            this.btnSplitPath.Size = new System.Drawing.Size(37, 30);
+            this.btnSplitPath.TabIndex = 5;
+            this.btnSplitPath.Text = "...";
+            this.btnSplitPath.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSplitPath.UseVisualStyleBackColor = true;
+            this.btnSplitPath.Click += new System.EventHandler(this.btnSplitPath_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Split Name:";
+            // 
+            // txtSplitName
+            // 
+            this.txtSplitName.Location = new System.Drawing.Point(131, 152);
+            this.txtSplitName.Name = "txtSplitName";
+            this.txtSplitName.Size = new System.Drawing.Size(573, 20);
+            this.txtSplitName.TabIndex = 7;
+            this.txtSplitName.Text = "FaceSheet";
+            // 
+            // btnSplitFiles
+            // 
+            this.btnSplitFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSplitFiles.Location = new System.Drawing.Point(32, 246);
+            this.btnSplitFiles.Name = "btnSplitFiles";
+            this.btnSplitFiles.Size = new System.Drawing.Size(715, 52);
+            this.btnSplitFiles.TabIndex = 9;
+            this.btnSplitFiles.Text = "Split Files";
+            this.btnSplitFiles.UseVisualStyleBackColor = true;
+            this.btnSplitFiles.Click += new System.EventHandler(this.btnSplitFiles_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(159, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(501, 42);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "KCEAA - SPLIT PDF FILES";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(131, 196);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(573, 41);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(780, 310);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSplitFiles);
+            this.Controls.Add(this.txtSplitName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSplitPath);
+            this.Controls.Add(this.textSplitPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "frmMain";
+            this.Text = "Main - Split PDFs - v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textSplitPath;
+        private System.Windows.Forms.Button btnSplitPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSplitName;
+        private System.Windows.Forms.Button btnSplitFiles;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ FTPSplitUtility/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace FTPSplitUtility
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = ".pdf";
+            openFileDialog1.Filter = "Pdf documents (.pdf)|*.pdf";
+            openFileDialog1.InitialDirectory = "S:\\RNDB\\CFD\\";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void btnSplitPath_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = "S:\\RNDB\\CFD\\Split\\";   // "S:\\RNDB\\CFD\\Split";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textSplitPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnSplitFiles_Click(object sender, EventArgs e)
+        {
+            string strCommand;
+
+            Cursor.Current = Cursors.WaitCursor;
+
+            strCommand = textBox1.Text + " burst output " + "\"" + textSplitPath.Text + "\\" + txtSplitName.Text + "_%04d.pdf\"";
+
+            var processInfo = new ProcessStartInfo("pdftk.exe", strCommand);
+
+            processInfo.CreateNoWindow = true;
+
+            processInfo.UseShellExecute = false;
+
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.Start();
+
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            //MOVE FILE TO PROCESSED
+            string path = textBox1.Text;
+            string path2 = textSplitPath.Text + "\\processed\\" + txtSplitName.Text + ".pdf";
+
+            if (!File.Exists(path))
+            {
+                // This statement ensures that the file is created,
+                // but the handle is not kept.
+                using (FileStream fs = File.Create(path)) { }
+            }
+
+            // Ensure that the target does not exist.
+            if (File.Exists(path2))
+                File.Delete(path2);
+
+            // Move the file.
+            File.Move(path, path2);
+            Console.WriteLine("{0} was moved to {1}.", path, path2);
+
+            Cursor.Current = Cursors.Default;
+
+            MessageBox.Show("Files were split to " + textSplitPath.Text, "SplitFiles Completion");
+
+
+        }
+    }
+}
